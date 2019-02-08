42fa8b544f3c1a37a2ed493fd48cef3df756fa02
Lucas Prieto <2324138p@student.gla.ac.uk>
2019-02-08 17:45:26 +0000

    chapter 9

--- rango/models.py
+++ rango/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
 
 class Category(models.Model):
@@ -24,3 +24,13 @@ class Page(models.Model):
 		return self.title
 
 
+class UserProfile(models.Model):
+	# This line is required. Links UserProfile to a User model instance.
+	user = models.OneToOneField(User)
+	# The additional attributes we wish to include.
+	website = models.URLField(blank=True)
+	picture = models.ImageField(upload_to='profile_images', blank=True)
+	# Override the __unicode__() method to return out something meaningful!
+	# Remember if you use Python 2.7.x, define __unicode__ too!
+	def __str__(self):
+		return self.user.username
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -16,8 +16,10 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR, ]
-MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [STATIC_DIR, ]
+
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -57,7 +59,8 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR, ],
+        'DIRS': [TEMPLATE_DIR, ]
+,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -65,7 +68,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.media'
+                'django.template.context_processors.media'
+
             ],
         },
     },
@@ -123,3 +127,8 @@ USE_TZ = True
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
+LOGIN_URL = '/rango/login/'
+PASSWORD_HASHERS = (
+'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
